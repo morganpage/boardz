0e8194ab32b3db5c71d8b128416ebb8824c8fb4b
Morgan Page <morganjpage@gmail.com>
2021-04-04 12:34:00 +0100

    ssl reject NOT

--- db.js
+++ db.js
@@ -11,7 +11,11 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const poolConfig = process.env.DATABASE_URL ?
-  { connectionString: process.env.DATABASE_URL } :
+  { connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  } :
   {
     user: 'postgres',
     password: 'postgres',
